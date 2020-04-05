ae7c3ed73d9aa39fdaeb31ba42a285affaadb8d5
cadecameron <cade.cameron@gmail.com>
2020-04-05 16:21:36 -0700

    UPDATED mySQL config file.

---
+++ db/mySQL/config.copy.js
@@ -0,0 +1,11 @@
+// Rename this file to config.js, and fill in the details below:
+
+const config = {
+  host: 'localhost',
+  user: '',
+  password: '',
+  database: '',
+  port: 3306,
+};
+
+module.exports = config;
