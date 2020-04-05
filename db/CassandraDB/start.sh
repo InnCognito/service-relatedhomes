b3707ee651db409104e1f658c36680d783563a0d
cadecameron <cade.cameron@gmail.com>
2020-04-05 16:59:22 -0700

    UPDATED for postgreSQL database option.

--- db/CassandraDB/start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-cd ~/temp/cassandra/bin
-# ./cassandra & BACK_PID=$!
-# wait $BACK_PID
-./cqlsh<<EOF
-SOURCE '/home/cade/ghrsea07/ghrsea07-system-design-capstone/service-relatedhomes/db/CassandraDB/cassandraSeedTest.cql'
-EOF
-exit 0
-
